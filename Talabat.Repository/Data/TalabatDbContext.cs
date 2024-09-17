9b82a5d58eb4b1499bdcb63f5153f571599f4350
MohamedAdel510 <119543542+MohamedAdel510@users.noreply.github.com>
2024-09-18 00:15:31 +0300

    Add project files.

---
+++ Talabat.Repository/Data/TalabatDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+
+namespace Talabat.Repository.Data
+{
+	public class TalabatDbContext : DbContext
+	{
+		public DbSet<Product> Products { get; set; }
+		public DbSet<ProductBrand> ProductBrands { get; set; }
+		public DbSet<ProductType> ProductTypes { get; set; }
+
+		public TalabatDbContext(DbContextOptions<TalabatDbContext> option) : base(option)
+		{
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+		}
+
+
+	}
+}
---
+++ Talabate.API/Program.cs
@@ -0,0 +1,64 @@
+using Microsoft.EntityFrameworkCore;
+using Talabat.Repository.Data;
+
+namespace Talabate.API
+{
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			#region Configuration Services
+			// Add services to the container.
+
+			builder.Services.AddControllers();
+			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+			builder.Services.AddEndpointsApiExplorer();
+			builder.Services.AddSwaggerGen();
+			builder.Services.AddDbContext<TalabatDbContext>(option =>
+			{
+				option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+			});
+			#endregion
+
+
+			var app = builder.Build();
+
+
+			#region Update-Database
+			using var Scope = app.Services.CreateScope();
+			var Services = Scope.ServiceProvider;
+			var LoggerFactory = Services.GetRequiredService<ILoggerFactory>();
+			try
+			{
+				var DbContext = Services.GetRequiredService<TalabatDbContext>();
+				await DbContext.Database.MigrateAsync();
+			}
+			catch (Exception ex)
+			{
+				var Logger = LoggerFactory.CreateLogger<Program>();
+				Logger.LogError(ex, "An error occured  during appling migration");
+			}
+			#endregion
+
+			#region Kestral Pipeline
+			// Configure the HTTP request pipeline.
+			if (app.Environment.IsDevelopment())
+			{
+				app.UseSwagger();
+				app.UseSwaggerUI();
+			}
+
+			app.UseHttpsRedirection();
+
+			app.UseAuthorization();
+
+
+			app.MapControllers();
+			#endregion
+
+			app.Run();
+		}
+	}
+}
